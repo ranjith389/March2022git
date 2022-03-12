2dbc52c7f14bd86573b536e6a5120e1cc45ea2d2
balayaranjith222@.gmail.com <mamathamammu1020@gmail.com>
2022-03-12 21:43:55 +0530

    iam commit the test

---
+++ src/main/java/selenum123/com/selenuim/automation/framewrok/App.java
@@ -0,0 +1,13 @@
+package selenum123.com.selenuim.automation.framewrok;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/selenum123/com/selenuim/automation/framewrok/Demo.java
@@ -0,0 +1,13 @@
+package selenum123.com.selenuim.automation.framewrok;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+   @Test
+	public void demo() {
+		System.out.println("@Test: demo()");
+
+	}
+	
+	}
+
